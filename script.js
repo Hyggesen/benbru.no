14f6e705e17e81b5d78aad029b6741e9ffecbcc7
Hyggesen <69785485+Hyggesen@users.noreply.github.com>
2021-01-11 13:00:17 +0100

    new updated javascript for changing language

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
+const intro = document.querySelector(".intro");
+const intro2 = document.querySelector (".intro2");
+const feHeader = document.querySelector (".frontend-text");
+const dmHeader = document.querySelector (".dm-space");
+const flagg = document.querySelector (".flagg");
+const norwegianIntro = "Jeg er en Front-End-utvikler student med bakgrunn fra Digital Markedsføring. Jeg har en lidenskap for å skape ting. Drømmen min er å jobbe fulltid med å lage moderne, responsiv og brukervennlige nettsider og applikasjoner. Jeg vil lære alt";
+const norwegianIntro2 = "Du kan kontakte meg på:"
+const norwegianFeHeader = "Front-End-Utvikler studiene";
+const norwegianDmHeader = "Digital Markedsføring studiene";
 
-var bytteKnapp = document.querySelector(".bytteKnapp p");
-var bodyText =document.querySelector(".body-text p");
-var reach = document.querySelector (".reach");
-var flagg = document.querySelector (".flagg");
-var feHeader = document.querySelector (".frontend-text");
-var dmHeader = document.querySelector (".dm-space");
 
+const languageButton = document.querySelector(".language-button");
+const englishIntro = intro.innerText;
+const englishIntro2 = intro2.innerText;
+const englishFeHeader = feHeader.innerText;
+const englishDmHeader = dmHeader.innerText;
+let languageVariable = "English";
 
-function changeLanguage () {
-    document.querySelector (".body-text p").innerHTML = "Jeg er en Front-End-utvikler student med bakgrunn fra Digital Markedsføring. Jeg har en lidenskap for å skape ting. Drømmen min er å jobbe fulltid med å lage moderne, responsiv og brukervennlige nettsider og applikasjoner. Jeg vil lære alt";
-    document.querySelector (".bytteKnapp p").innerHTML = "Change to English";
-    document.querySelector (".reach").innerHTML = "Du kan kontakte meg på:";
-    document.querySelector (".frontend-text").innerHTML = "Front-End-Utvikler studiene";
-    document.querySelector (".dm-space").innerHTML = "Digital Markedsføring studiene"
-    document.getElementById("flagg") .src="united-kingdom.png";
+languageButton.addEventListener("click", function(){
+  if(languageVariable === "English"){
+    
+    intro.innerHTML = norwegianIntro;
+    intro2.innerHTML = norwegianIntro2;
+    feHeader.innerHTML = norwegianFeHeader;
+    dmHeader.innerHTML = norwegianDmHeader;
+    languageButton.innerText = "Change to English";
+    languageVariable = "Norwegian";
 
-}
-
-bytteKnapp.onclick = changeLanguage;
-
+  }
+  else {
+        intro.innerHTML = englishIntro;
+        intro2.innerHTML = englishIntro2;
+        feHeader.innerHTML = englishFeHeader;
+        dmHeader.innerHTML = englishDmHeader;
+        
+      languageButton.innerText = "Bytt til Norsk";
+      languageVariable = "English";
+  }
+})
