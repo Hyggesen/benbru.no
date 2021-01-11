2110b52a4261413e4e5e2db41d0f5fca1a81f0c7
Hyggesen <69785485+Hyggesen@users.noreply.github.com>
2021-01-11 10:36:22 +0100

    removed some more js

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ var dmHeader = document.querySelector (".dm-space");
 function changeLanguage () {
     document.querySelector (".body-text p").innerHTML = "Jeg er en Front-End-utvikler student med bakgrunn fra Digital Markedsføring. Jeg har en lidenskap for å skape ting. Drømmen min er å jobbe fulltid med å lage moderne, responsiv og brukervennlige nettsider og applikasjoner. Jeg vil lære alt";
     document.querySelector (".bytteKnapp p").innerHTML = "Change to English";
-    document.querySelector (".bytteKnapp p").classList.add("byttTilbake");
     document.querySelector (".reach").innerHTML = "Du kan kontakte meg på:";
     document.querySelector (".frontend-text").innerHTML = "Front-End-Utvikler studiene";
     document.querySelector (".dm-space").innerHTML = "Digital Markedsføring studiene"
