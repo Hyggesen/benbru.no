6bf5e7ce31865558105e4847bf14f2a5c7445d04
Hyggesen <69785485+Hyggesen@users.noreply.github.com>
2021-01-11 10:25:55 +0100

    small text change

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var dmHeader = document.querySelector (".dm-space");
 
 
 function changeLanguage () {
-    document.querySelector (".body-text p").innerHTML = "Jeg er en Front-End-utvikler student med en bakgrunn fra Digital Markedsføring. Jeg har en lidenskap for å skape ting. Drømmen min er å jobbe fulltid med å lage moderne, responsiv og brukervennlige nettsider og applikasjoner. Jeg vil lære alt";
+    document.querySelector (".body-text p").innerHTML = "Jeg er en Front-End-utvikler student med bakgrunn fra Digital Markedsføring. Jeg har en lidenskap for å skape ting. Drømmen min er å jobbe fulltid med å lage moderne, responsiv og brukervennlige nettsider og applikasjoner. Jeg vil lære alt";
     document.querySelector (".bytteKnapp p").innerHTML = "Change to English";
     document.querySelector (".reach").innerHTML = "Du kan kontakte meg på:";
     document.querySelector (".frontend-text").innerHTML = "Front-End-Utvikler studiene";
